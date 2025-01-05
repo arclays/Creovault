a43082114ed3d575bb3da9e76d28be3b95e39f0a
dansan-dsn <ddryn970@gmail.com>
2025-01-05 21:12:53 +0300

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,23 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import Navbar from "./components/Navbar";
+import Movies from "./pages/Movies";
+import Books from "./pages/Books";
+import Software from "./pages/Software";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="min-h-screen text-white bg-[#1a1a1a] transition-all duration-300 ease-in-out">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/books" element={<Books />} />
+          <Route path="/software" element={<Software />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
